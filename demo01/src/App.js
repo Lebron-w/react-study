2418ef0f371c265827dff9c6cc34939555165b04
Lebron-w <1264013652@qq.com>
2020-11-03 07:18:14 +0800

    JSX语法简单介绍

--- demo01/src/App.js
+++ demo01/src/App.js
@@ -3,10 +3,15 @@ import React, {Component} from 'react'
 class App extends Component {
     render () {
         return (
-            <div>
-                hello Jspang
-            </div>
+            //JSX
+            <ul className="my-list">
+                <li>{false ? 'Js.com' : '魏'}</li>
+                <li>I Love React</li>
+            </ul>
         )
+        // var child1 = React.createElement('li',null,'js.com')
+        // var child2 = React.createElement('li',null,'I Love React')
+        // var root = React.createElement('ul',{className:'my-list'},child1,child2)
     }
 }
 export default App
--- demo01/src/index.js
+++ demo01/src/index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import App from './App'
 
-
+// JSX: javascript and xml < {
 ReactDom.render(<App/>,document.getElementById('root'))
