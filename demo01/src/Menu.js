7ccd698bef8f7d2a231b4c2337500cc03df41b28
Lebron-w <1264013652@qq.com>
2020-11-03 08:11:43 +0800

    响应式设计

--- demo01/src/Menu.js
+++ demo01/src/Menu.js
@@ -1,13 +1,20 @@
 import React, {Component,Fragment} from 'react'
 
 class Menu extends Component{
+    constructor (props) {
+        super(props) //调用父级的方法
+        this.state = {
+            inputValue: 'js',
+            list: []
+        }
+    }
     render () {
         return (
             // flex
             <Fragment>
                 <div>
-                    <input/>
-                    <button>导航</button>
+                    <input value={this.state.inputValue} onChange={this.inputChange.bind(this)}/>
+                    <button>增加导航</button>
                 </div>
                 <ul>
                     <li>about</li>
@@ -15,6 +22,13 @@ class Menu extends Component{
                 </ul>
             </Fragment>
         )
+    }
+    inputChange (e) {
+        // console.log(e.target.value)
+        // console.log(this)
+        this.setState({
+            inputValue:e.target.value
+        })
     }
 }
 export default Menu
